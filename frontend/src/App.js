3eea9c8eb5fa80e80d93dc929cfbf4fa97170909
Charles Magalhães <charlesrm29@gmail.com>
2021-06-20 01:27:00 -0300

    Alterações no frontend devido a erros

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import ButtonContainer from './components/ButtonContainer';
-import * as api from './api/api';
-import Spinner from './components/Spinner';
-import TodosControl from './components/TodosControl';
+import React, { Component } from 'react';
+import ProjetoBase from './components/ProjetoBase/ProjetoBase';
 
-
-export default function () {
-  const [selectedYer, setSelectedYer] = useState([]);
-  const [selectedMonth, setSelecedMonth]= useState({});
-  const [selectedTodos, setSelectedTodos]= useState([]);
-
-
-
- const testAp = async() =>{
-    const result = await api.getAllTodo();
-    console.log(result);
-   return result;
-  
- } 
- 
-//  useEffect(() => {
-//   api.getAllTodo().then((todos) => {
-//     setTimeout(() => {
-//       selectedTodos(todos);
-//     }, 2000);
-//   });
-//  }, [])
-
- useEffect(() => {
-   const getTodos = async () => {
-    const todos = await api.getAllTodo();
-    setTimeout(() => {
-      setSelectedTodos(todos);
-    }, 2000);
-   };
-
-   getTodos();
-
- }, [])
- 
-
-testAp();
-
-
-    return (
-      <div>
-
-        <ButtonContainer/>
-        {selectedTodos.length == 0 && <Spinner/>}
-        {selectedTodos.length > 0 && <TodosControl todos ={selectedTodos}/> }
-      </div>
-
-    );
+export default class App extends Component {
+  render() {
+    return <ProjetoBase />;
+  }
 }
