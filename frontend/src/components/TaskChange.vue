b1d4c2397030eb94990f788b20b110a06a4f4477
Charles <charles.magalhaes@animaeducacao.com.br>
2021-06-30 11:40:18 -0300

    Criação do TaskChange

--- frontend/src/components/TaskChange.vue
+++ frontend/src/components/TaskChange.vue
@@ -1,7 +1,4 @@
 <template>
-  <div class="changeCss">
-    xxxxxxxxxxxxxxx
-  </div>
 </template>
 
 <script>
@@ -14,6 +11,18 @@ export default {
 .changeCss {
   width: 500px;
   background: #AAAAAA;
+}
+
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
 }
 
 </style>
