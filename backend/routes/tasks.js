53173e2e1c37fb16d0c200b810a6e987245eccaf
Charles Magalhães <charlesrm29@gmail.com>
2021-07-06 23:56:03 -0300

    Criado método Get

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -9,13 +9,22 @@ router.post('/', async (req, res) => {
     let task = req.body;
     const data = JSON.parse(await readFile('tasks.json'));
 
-    task.id = data.nextId;
-    data.nextId++;
+    task = { id: data.nextId++, ...task };
     data.tasks.push(task);
 
-    await writeFile('tasks.json', JSON.stringify(data));
+    await writeFile('tasks.json', JSON.stringify(data, null, 2));
 
     res.send(task);
+  } catch (error) {
+    res.status(400).send({ erro: error.message });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile('tasks.json'));
+    console.log(data);
+    res.send(data);
   } catch (error) {
     res.status(400).send({ erro: error.message });
   }
